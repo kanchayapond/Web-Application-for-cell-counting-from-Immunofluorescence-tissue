1e6d1649b9275f2ef85ee71c2a18fdb68744a8b6
Earnn <earnjoong@gmail.com>
2023-02-19 14:38:52 +0700

    bugg

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ data = {'Nucleus': [1, 2, 3, 4],
         'Area': [20, 21, 19, 18]}
 df_result = pd.DataFrame(data)
 
-@st.cache
+@st.cache_data
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
